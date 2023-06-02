c9af0d17bef8795872d08e19e63e28713890cca3
Darren LaCour <lacourd@icloud.com>
2023-06-02 11:33:49 -0500

    Midway through Part 4

--- queries.sql
+++ queries.sql
@@ -6,7 +6,8 @@ SELECT * FROM techjobs.job;
 --skills:varchar(255)
 
 -- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
