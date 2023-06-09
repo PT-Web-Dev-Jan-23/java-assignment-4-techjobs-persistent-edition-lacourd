e0796a27b7f838ad00ec2ea606ded2a51d90041b
Darren LaCour <lacourd@icloud.com>
2023-06-08 19:42:36 -0500

    Fixed that jobs view wasn't rendering

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -74,6 +74,13 @@ public class HomeController {
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
+        Optional<Job> result= jobRepository.findById(jobId);
+        if (result.isPresent()) {
+            Job job = result.get();
+            model.addAttribute("job", job);
+        } else {
+            return "index";
+        }
 
         return "view";
     }
